a9f32ca244bdaa57095ed10f7dfde4dfdcbed119
jacksmithinsulander <666.jack.smith@protonmail.com>
2024-05-25 11:16:15 +0200

    more boilerplate

--- src/SBFModule.sol
+++ src/SBFModule.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract BKModule {
+import {ISafe} from "@gnosis/contracts/interfaces/ISafe.sol";
+
+contract SBFModule {
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
---
+++ test/SBFModule.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {SBFModule} from "src/SBFModule.sol";
+
+contract SBFTest is Test {
+}
