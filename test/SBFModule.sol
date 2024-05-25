5e77a435f315a5a1e056bb27a9dfacfaa34cc5fe
jacksmithinsulander <666.jack.smith@protonmail.com>
2024-05-25 18:09:30 +0200

    added smal test

--- test/SBFModule.sol
+++ test/SBFModule.sol
@@ -4,14 +4,40 @@ pragma solidity 0.8.19;
 import {Test, console} from "forge-std/Test.sol";
 import {SBFModule} from "src/SBFModule.sol";
 
+/* Mock Contracts */
+import {MockToken} from "test/mock/MockToken.sol";
+
 contract SBFTest is Test {
 
-    address token;
-    address deployer;
-    address sponsor;
+    address public deployer;
+    address public sponsor;
+
+    SBFModule public sbf;
+    MockToken public token;
+
 
     function setUp() external {
-        token = address(0x01);
+
+        deployer = address(0x2);
+        sponsor = address(0x3);
+
+        token = new MockToken("token", "MTK");
+
+        token.mint(deployer, 5_000e6);
+        token.mint(sponsor, 5_000e6);
+
+        vm.prank(deployer);
+        sbf = new SBFModule(address(token));
+    }
+
+    function test_depositBounty() external {
+        vm.startPrank(sponsor);
+
+        token.approve(address(sbf), 5_000e6);
+
+        sbf.depositBounty("asdfkl", 5_000e6);
+
+        vm.stopPrank();
     }
 
 }
